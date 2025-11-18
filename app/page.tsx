d94d0559005e3a67a7d5c86c4149f6337f3a3486
Naqash879 <naqashworkbench@gmail.com>
2025-11-18 19:53:27 +0000

    After Reviewed by myself

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,22 @@ import SearchBar from "@/components/SearchBar";
 function Home() {
   return (
     <div className="w-screen h-screen">
-      <SearchBar />
-      <div className="main ml-20 mt-12 ">
-        <span className="font-semibold text-black">Current Watching</span>
-        <div className="flex mt-3">
-          <div className="columns-2 gap-6">
+      {<SearchBar />}
+      <div className="mt-10 md:mt-20 main md:ml-15">
+        <span className="text-[20px] text-black ml-4 md:ml-5">
+          Currently Watching
+        </span>
+        <div className="flex mt-0.5 gap-4 sm:mt-4">
+          <div className="pl-[20px]">
             <img
-              className="w-[177px] h-[263px] rounded-3xl"
+              className="w-[158px] h-[234px] md:w-[177px] md:h-[263px] rounded-2xl"
               src="/images/Component1.png"
               alt="component1"
             />
+          </div>
+          <div className="">
             <img
-              className="w-[177px] h-[263px] rounded-3xl"
+              className="w-[158px] h-[234px] md:w-[177px] md:h-[263px] rounded-2xl"
               src="/images/Component2.png"
               alt="component2"
             />
