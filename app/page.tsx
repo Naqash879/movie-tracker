7e4a2577aba359adc1c7fde2bf8c9ab77fd2f1e0
Naqash879 <naqashworkbench@gmail.com>
2025-11-19 18:22:19 +0000

    86evh89xm suggested to watch (#6)
    * Review Search bar
    
    * Review Current Waching Ticket
    
    * After Reviewed by myself
    
    * resolved changes
    
    * successfully created 86evh89xm-suggested-to-watch
    
    * Review for 86evh89xm-suggested-to-watch
    
    * Resolved Issue
    
    * Resolved

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,38 @@
 "use client";
 import "./globals.css";
 import SearchBar from "@/components/SearchBar";
+import ProjectImages from "@/components/ProjectImages";
 
-function Home() {
+export default function Home() {
   return (
-    <div className="w-screen h-screen border-amber-300 border-  4">
+    <div className="w-full min-h-screen px-4 lg:px-10">
       <SearchBar />
-      <div className="mt-10 md:mt-20 main md:ml-15">
-        1
-        <span className="text-[20px] text-black ml-4 md:ml-5">
-          Currently Watching
-        </span>
-        <div className="flex mt-0.5 gap-4 sm:mt-4">
-          <div className="pl-[20px]">
-            <img
-              className="w-[158px] h-[234px] md:w-[177px] md:h-[263px] rounded-2xl"
-              src="/images/Component1.png"
-              alt="component1"
-            />
+
+      <div className="mt-6 sm:mt-10 flex flex-col sm:flex-row sm:justify-between sm:items-start gap-6">
+        <div className="flex-1">
+          <h2 className="text-[20px] font-bold mb-3">Currently Watching</h2>
+          <div className="flex gap-4 overflow-x-auto max-w-full">
+            <div className="flex gap-4 flex-nowrap">
+              <ProjectImages src="/images/Component1.png" alt="Component1" />
+              <ProjectImages src="/images/Component2.png" alt="Component2" />
+            </div>
           </div>
-          <div className="">
-            <img
-              className="w-[158px] h-[234px] md:w-[177px] md:h-[263px] rounded-2xl"
-              src="/images/Component2.png"
-              alt="component2"
-            />
+        </div>
+        <div className="hidden sm:flex-1 sm:flex sm:flex-col sm:items-start">
+          <h2 className="text-[20px] font-bold mb-3 text-left">
+            Suggested To Watch
+          </h2>
+          <div className="flex gap-4 flex-wrap sm:flex-nowrap sm:overflow-x-auto w-full">
+            <div className="flex gap-4 flex-wrap sm:flex-nowrap w-max ">
+              <ProjectImages src="/images/Component1.png" alt="Component1" />
+              <ProjectImages src="/images/Component2.png" alt="Component2" />
+              <ProjectImages src="/images/Component3.png" alt="Component3" />
+              <ProjectImages src="/images/Component4.png" alt="Component4" />
+              <ProjectImages src="/images/Component5.png" alt="Component5" />
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 }
-export default Home;
---
+++ components/ProjectImages.jsx
@@ -0,0 +1,9 @@
+function ProjectImages({ ...props }) {
+  return (
+    <img
+      className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
+      {...props}
+    />
+  );
+}
+export default ProjectImages;
