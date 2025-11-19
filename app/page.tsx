29e8f3c10c893a29548fd531fd250c8bd774bd49
Naqash879 <naqashworkbench@gmail.com>
2025-11-19 17:04:48 +0000

    Resolved Issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import "./globals.css";
 import SearchBar from "@/components/SearchBar";
+import ProjectImages from "@/components/ProjectImages";
 
 export default function Home() {
   return (
@@ -9,48 +10,25 @@ export default function Home() {
 
       <div className="mt-6 sm:mt-10 flex flex-col sm:flex-row sm:justify-between sm:items-start gap-6">
         <div className="flex-1">
-          <h2 className="text-[22px] font-bold mb-3">Currently Watching</h2>
+          <h2 className="text-[20px] font-bold mb-3">Currently Watching</h2>
           <div className="flex gap-4 overflow-x-auto max-w-full">
             <div className="flex gap-4 flex-nowrap">
-              <img
-                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
-                src="/images/Component1.png"
-                alt=""
-              />
-              <img
-                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
-                src="/images/Component2.png"
-                alt=""
-              />
+              <ProjectImages src="/images/Component1.png" alt="Component1" />
+              <ProjectImages src="/images/Component2.png" alt="Component2" />
             </div>
           </div>
         </div>
         <div className="hidden sm:flex-1 sm:flex sm:flex-col sm:items-start">
-          <h2 className="text-[22px] font-bold mb-3 text-left">
+          <h2 className="text-[20px] font-bold mb-3 text-left">
             Suggested To Watch
           </h2>
-          <div className="overflow-x-auto w-full">
-            <div className="flex gap-4 flex-wrap">
-              <img
-                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
-                src="/images/Component1.png"
-                alt=""
-              />
-              <img
-                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
-                src="/images/Component2.png"
-                alt=""
-              />
-              <img
-                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
-                src="/images/Component3.png"
-                alt=""
-              />
-              <img
-                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
-                src="/images/Component4.png"
-                alt=""
-              />
+          <div className="flex gap-4 flex-wrap sm:flex-nowrap sm:overflow-x-auto w-full">
+            <div className="flex gap-4 flex-wrap sm:flex-nowrap w-max ">
+              <ProjectImages src="/images/Component1.png" alt="Component1" />
+              <ProjectImages src="/images/Component2.png" alt="Component2" />
+              <ProjectImages src="/images/Component3.png" alt="Component3" />
+              <ProjectImages src="/images/Component4.png" alt="Component4" />
+              <ProjectImages src="/images/Component5.png" alt="Component5" />
             </div>
           </div>
         </div>
