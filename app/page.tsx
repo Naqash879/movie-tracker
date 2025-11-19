44001cc8194b738e629f58489d4a3d70d3cce100
Naqash879 <naqashworkbench@gmail.com>
2025-11-19 14:15:07 +0000

    Review for 86evh89xm-suggested-to-watch

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,59 @@
 import "./globals.css";
 import SearchBar from "@/components/SearchBar";
 
-function Home() {
+export default function Home() {
   return (
-    <div className="w-screen h-screen border-amber-300 border-  4">
+    <div className="w-full min-h-screen px-4 lg:px-10">
       <SearchBar />
-      <div className="mt-10 md:mt-20 main md:ml-15">
-        <span className="text-[20px] text-black ml-4 md:ml-5">
-          Currently Watching
-        </span>
-        <div className="flex mt-0.5 gap-4 sm:mt-4">
-          <div className="pl-[20px]">
-            <img
-              className="w-[158px] h-[234px] md:w-[177px] md:h-[263px] rounded-2xl"
-              src="/images/Component1.png"
-              alt="component1"
-            />
+
+      <div className="mt-6 sm:mt-10 flex flex-col sm:flex-row sm:justify-between sm:items-start gap-6">
+        <div className="flex-1">
+          <h2 className="text-[22px] font-bold mb-3">Currently Watching</h2>
+          <div className="flex gap-4 overflow-x-auto max-w-full">
+            <div className="flex gap-4 flex-nowrap">
+              <img
+                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
+                src="/images/Component1.png"
+                alt=""
+              />
+              <img
+                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
+                src="/images/Component2.png"
+                alt=""
+              />
+            </div>
           </div>
-          <div className="">
-            <img
-              className="w-[158px] h-[234px] md:w-[177px] md:h-[263px] rounded-2xl"
-              src="/images/Component2.png"
-              alt="component2"
-            />
+        </div>
+        <div className="hidden sm:flex-1 sm:flex sm:flex-col sm:items-start">
+          <h2 className="text-[22px] font-bold mb-3 text-left">
+            Suggested To Watch
+          </h2>
+          <div className="overflow-x-auto w-full">
+            <div className="flex gap-4 flex-wrap">
+              <img
+                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
+                src="/images/Component1.png"
+                alt=""
+              />
+              <img
+                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
+                src="/images/Component2.png"
+                alt=""
+              />
+              <img
+                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
+                src="/images/Component3.png"
+                alt=""
+              />
+              <img
+                className="w-[158px] h-[234px] lg:w-[177px] lg:h-[263px] rounded-2xl flex-shrink-0"
+                src="/images/Component4.png"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 }
-export default Home;
