d29cc16ab6a4835c19553dcb6436c35dbddfe064
Naqash879 <naqashworkbench@gmail.com>
2025-11-24 13:05:13 +0000

    Created Search Page

---
+++ app/searchdata/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import ProjectImages from "@/components/ProjectImages";
+import { useState } from "react";
+import SearchBar from "@/components/SearchBar";
+import { DynamicSearchData, Movie } from "@/utils/data";
+export default function SearchData() {
+  const [DynamicSearchDataState] = useState<Movie[]>(DynamicSearchData);
+  return (
+    <div className="w-screen px-5 h-screen sm:px-5 md:px-5 lg:px-20 ">
+      <SearchBar />
+      <div>
+        <p className="hidden sm:block text-[15px] font-bold mt-15">
+          Showing Search result for:
+          <span className="text-[20px] text-gray-400"> Incap</span>
+        </p>
+        <div className="flex h-full mt-5 sm:mt-3">
+          <div className="flex flex-wrap gap-4 md:flex-wrap">
+            {DynamicSearchDataState.map((search) => (
+              <ProjectImages
+                key={search.id}
+                src={search.src}
+                alt={search.alt}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
