8f2e5072441d7d67314df31bd4f00b8451ab9dab
Naqash879 <naqashworkbench@gmail.com>
2025-11-24 14:33:27 +0000

    Change IframMovie Trial to MovieTrailer and also Probs to props

---
+++ components/MovieTrailer.jsx
@@ -0,0 +1,13 @@
+function MovieTrailer({ ...props }) {
+  return (
+    <iframe
+      src="https://www.youtube.com/embed/BmllggGO4pM"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      {...props}
+    />
+  );
+}
+export default MovieTrailer;
