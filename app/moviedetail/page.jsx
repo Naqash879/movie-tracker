d5adc4ed79d9dec66d97bc3be4cc63ca2624452d
Naqash879 <naqashworkbench@gmail.com>
2025-11-24 14:31:54 +0000

    86evkrdx2 movie page (#9)
    * Review Search bar
    
    * Review Current Waching Ticket
    
    * After Reviewed by myself
    
    * resolved changes
    
    * successfully created 86evh89xm-suggested-to-watch
    
    * Review for 86evh89xm-suggested-to-watch
    
    * Resolved Issue
    
    * Resolved
    
    * Review for Previously Watch
    
    * Exerciese Json
    
    * make a dynamicdata for mages
    
    * Resolved Issues
    
    * Resolved Issues through typescript
    
    * Resolved Issues through typescript(1.2)
    
    * Movie Page Created
    
    * Review Movie page
    
    * Resolved Issues
    
    * Revert "Review Movie page"
    
    This reverts commit b48683119cd5e9ce151ca6952b98a0f4ed9bd631.
    
    * Resolved Issue(1.2)
    
    * Revert "Resolved Issue(1.2)"
    
    This reverts commit 2488bedb58330f844776850d7f7343ef40bb96d0.
    
    * Latest Resolved
    
    * Resolved Issue 1.3
    
    * made component for movie trial
    
    * Change IframMovie Trial to MovieTrailer and also Probs to props
    
    * Change IframMovie Trial to MovieTrailer and also Probs to props

---
+++ app/moviedetail/page.jsx
@@ -0,0 +1,96 @@
+"use client";
+import SearchBar from "@/components/SearchBar";
+import MovieTrailer from "@/components/MovieTrailer";
+function MovieDetail() {
+  return (
+    <div className="w-screen h-screen  px-20">
+      <SearchBar />
+      <h1 className=" text-3xl font-bold mt-10 shrink-0">The Grey Man</h1>
+      <div className="flex flex-col mt-3 sm:hidden w-full ">
+        <div className="flex relative px-5">
+          <MovieTrailer className="w-full h-[300px] rounded-lg" />
+          <div className="flex items-start justify-baseline absolute bottom-0 translate-y-10 left-4 ">
+            <img
+              src="/images/component1.png "
+              className="w-[104px] h-[150px] rounded-2xl mx-auto  shadow-lg  ml-5"
+            />
+          </div>
+        </div>
+        <div className="flex gap-4 mt-17">
+          <button className="px-4 py-1 bg-gray-200 rounded-4xl text-black border border-black-600">
+            Action
+          </button>
+          <button className="px-4 py-1 bg-gray-200 rounded-4xl text-black border border-black-600">
+            Sci-Fi
+          </button>
+        </div>
+        <div>
+          <div className="w-full h-auto mt-8">
+            <p className="text-lg">
+              A thief who steals corporate secrets through the use of
+              dream-sharing technology is given the inverse task of planting an
+              idea into the mind of a C.E.O., but his tragic past may doom the
+              project and his team to disaster.
+            </p>
+          </div>
+          <div className="flex mt-8">
+            <p className="text-lg text-black">IMDB Rating</p>
+            <span
+              className="py-6 ml-4  text-sm text-gray-600
+                "
+            >
+              8k Reviews
+            </span>
+          </div>
+          <p className="text-lg">
+            ⭐ 9.1/<span className="text-sm text-gray-400">10</span>
+          </p>
+        </div>
+      </div>
+      <div className="hidden sm:flex sm:flex-wrap sm:flex-col sm:mt-3 md:flex-col lg:flex-row  ">
+        <div className="flex flex-1 sm:flex-row  md:flex-row  gap-6">
+          <div className="shrink-0">
+            <img
+              className="w-[196px] h-[291px] object-cover rounded-2xl"
+              src="/images/component1.png"
+            />
+          </div>
+          <div className="flex flex-col gap-4 mt-5 ">
+            <div className="flex gap-4">
+              <button className="px-4 py-1 bg-gray-200 rounded-4xl text-black border border-black-600">
+                Action
+              </button>
+              <button className="px-4 py-1 bg-gray-200 rounded-4xl text-black border border-black-600">
+                Sci-Fi
+              </button>
+            </div>
+            <div className="w-[413px]">
+              <p className="text-lg break-words">
+                A thief who steals corporate secrets through the use of
+                dream-sharing technology is given the inverse task of planting
+                an idea into the mind of a C.E.O., but his tragic past may doom
+                the project and his team to disaster.
+              </p>
+            </div>
+            <div>
+              <div className="flex items-center">
+                <p className="text-lg text-black">IMDB Rating</p>
+                <span className="p-6 ml-4 text-sm text-gray-600">
+                  8k Reviews
+                </span>
+              </div>
+              <p className="text-lg">
+                ⭐ 9.1/<span className="text-sm text-gray-400">10</span>
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-1  h-[300px] items-end justify-end">
+            <MovieTrailer className="w-[521px] h-[300px] rounded-lg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default MovieDetail;
--- utils/data.ts
+++ utils/data.ts
@@ -29,4 +29,22 @@ export const previouslyWatched: Movie[] = [
   { id: 10, alt: "component5", src: "/images/component5.png" },
   { id: 11, alt: "component4", src: "/images/component4.png" },
   { id: 12, alt: "component3", src: "/images/component3.png" },
+  { id: 13, alt: "component2", src: "/images/component2.png" },
+  { id: 14, alt: "component1", src: "/images/component1.png" },
+];
+export const DynamicSearchData: Movie[] = [
+  { id: 1, alt: "component7", src: "/images/component7.png" },
+  { id: 2, alt: "component6", src: "/images/component6.png" },
+  { id: 3, alt: "component5", src: "/images/component5.png" },
+  { id: 4, alt: "component4", src: "/images/component4.png" },
+  { id: 5, alt: "component3", src: "/images/component3.png" },
+  { id: 6, alt: "component2", src: "/images/component2.png" },
+  { id: 7, alt: "component1", src: "/images/component1.png" },
+  { id: 8, alt: "component7", src: "/images/component7.png" },
+  { id: 9, alt: "component6", src: "/images/component6.png" },
+  { id: 10, alt: "component5", src: "/images/component5.png" },
+  { id: 11, alt: "component4", src: "/images/component4.png" },
+  { id: 12, alt: "component3", src: "/images/component3.png" },
+  { id: 13, alt: "component2", src: "/images/component2.png" },
+  { id: 14, alt: "component1", src: "/images/component1.png" },
 ];
