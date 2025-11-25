52510c80b54eae4dcb204fd832a9ddecf6ed126d
Naqash879 <naqashworkbench@gmail.com>
2025-11-25 13:10:21 +0000

    Toater are created and applied

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,10 +1,12 @@
 "use client";
+
 import Link from "next/link";
 import InputField from "@/components/InputField";
 import { useState } from "react";
 import FormButton from "@/components/FormButton";
 import { register } from "@/services/user";
-import { useRouter } from "next/navigation";
+import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 function Signup() {
   const [firstName, setFirstName] = useState("");
@@ -12,22 +14,18 @@ function Signup() {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [success, setSuccess] = useState(false);
-  const [message, setMessage] = useState("");
-
-  const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const loadingToast = toast.loading("Loading...");
+
     if (password !== confirmPassword) {
-      setErrorMessage("Password and Confirm Password did not match");
+      toast.dismiss(loadingToast);
+      toast.error("Password and Confirm Password did not match");
       return;
     }
 
-    setErrorMessage("");
-    
     const formattedFirstName =
       firstName.trim().charAt(0).toUpperCase() +
       firstName.trim().slice(1).toLowerCase();
@@ -36,96 +34,94 @@ function Signup() {
       lastName.trim().charAt(0).toUpperCase() +
       lastName.trim().slice(1).toLowerCase();
 
-    register(formattedFirstName, formattedLastName, email, password)
-      .then((data) => {
-        if (data.success) {
-          setMessage(data.message);
-          setSuccess(true);
+    try {
+      const data = await register(
+        formattedFirstName,
+        formattedLastName,
+        email,
+        password
+      );
 
-          router.push("/login");
-        } else {
-          setMessage(data.message);
-          setSuccess(false);
-        }
-      })
-      .catch((error) => alert(error));
+      toast.dismiss(loadingToast);
+
+      if (data.success) {
+        toast.success(data.message);
+        // router.push("/login");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.dismiss(loadingToast);
+      toast.error(error);
+    }
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="pt-10 mx-11 w-[293px] h-[100.79px] sm:w-[393px] sm:mx-40 md:w-[393px] md:mx-80 lg:w-[393px] lg:mx-100 xl:w-[393px] xl:mx-110 2xl:w-[393px] 2xl:mx-200 2xl:my-30">
-        {message && (
-          <div
-            className={`w-full text-center p-5 rounded-lg text-white ${
-              success ? "bg-green-500" : "bg-red-500"
-            }`}
-          >
-            {message}
-          </div>
-        )}
+    <>
+      <Toaster />
 
-        <h1 className="text-[40px] font-bold text-center">Movie Maker</h1>
+      <div className="flex flex-col">
+        <div className="pt-10 mx-11 w-[293px] h-[100.79px] sm:w-[393px] sm:mx-40 md:w-[393px] md:mx-80 lg:w-[393px] lg:mx-100 xl:w-[393px] xl:mx-110 2xl:w-[393px] 2xl:mx-200 2xl:my-30">
+          <h1 className="text-[40px] font-bold text-center">Movie Maker</h1>
 
-        <form className="flex flex-col mt-8 gap-2" onSubmit={handleSubmit}>
-          <InputField
-            placeholder="First Name"
-            name="firstname"
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value.trim())}
-            required
-          />
+          <form className="flex flex-col mt-8 gap-2" onSubmit={handleSubmit}>
+            <InputField
+              placeholder="First Name"
+              name="firstname"
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value.trim())}
+              required
+            />
 
-          <InputField
-            placeholder="Last Name"
-            name="lastname"
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value.trim())}
-            required
-          />
+            <InputField
+              placeholder="Last Name"
+              name="lastname"
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value.trim())}
+              required
+            />
 
-          <InputField
-            placeholder="Email"
-            name="email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value.trim())}
-            required
-          />
+            <InputField
+              placeholder="Email"
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value.trim())}
+              required
+            />
 
-          <InputField
-            placeholder="Password"
-            name="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+            <InputField
+              placeholder="Password"
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
 
-          <InputField
-            placeholder="Confirm Password"
-            name="confirm-password"
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
+            <InputField
+              placeholder="Confirm Password"
+              name="confirm-password"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
 
-          <FormButton type="submit">Sign Up</FormButton>
-        </form>
-        <div className="text-lg text-red-800 flex justify-center">
-          {errorMessage}
+            <FormButton type="submit">Sign Up</FormButton>
+          </form>
+
+          <span className="mt-5 ml-5 text-[15px]">
+            Already have an account?{" "}
+            <Link href="/login" className="font-semibold text-gray-600">
+              Login
+            </Link>
+          </span>
         </div>
-
-        <span className="mt-5 ml-5 text-[15px]">
-          Already have an account?{" "}
-          <Link href="/login" className="font-semibold text-gray-600">
-            Login
-          </Link>
-        </span>
       </div>
-    </div>
+    </>
   );
 }
 
