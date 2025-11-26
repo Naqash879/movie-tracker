77830971198eae7799ed80f5f3c0dc580db6b46d
Naqash879 <naqashworkbench@gmail.com>
2025-11-26 13:14:43 +0000

    Fixed image stretch issue

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <div className="mt-6 sm:mt-10 flex flex-col sm:flex-row sm:justify-between sm:items-start gap-6 w-full">
           <div className="flex-1">
             <h2 className="text-[20px] font-bold mb-3">Currently Watching</h2>
-            <div className="flex gap-4 overflow-x-auto max-w-full">
+            <div className="flex gap-4 max-w-full">
               <div className="flex gap-4 flex-nowrap shrink-0">
                 {currentlyWatchingList.map((curr) => (
                   <ProjectImages key={curr.id} src={curr.src} alt={curr.alt} />
@@ -43,10 +43,10 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="w-[400px] md:w-full mt-6 sm:mt-10 md:overflow-x-auto">
+        <div className="w-[400px] md:w-full mt-6 sm:mt-10 md:flex-wrap">
           <h2 className="text-[20px] font-bold mb-3">Previously Watched</h2>
 
-          <div className="flex w-full gap-4 flex-wrap md:flex-nowrap md:cursor-grab md:active:cursor-grabbing">
+          <div className="flex gap-4 md:flex-wrap">
             {previouslyWatchedList.map((prev) => (
               <ProjectImages key={prev.id} src={prev.src} alt={prev.alt} />
             ))}
