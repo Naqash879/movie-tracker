5e289b80f017a3a4e41f8af50ce4ca929a4cf348
Naqash879 <naqashworkbench@gmail.com>
2025-11-26 13:49:58 +0000

    apply object-fit property to components

--- utils/data.ts
+++ utils/data.ts
@@ -6,14 +6,14 @@ export type Movie = {
 
 export const currentlyWatching: Movie[] = [
   { id: 1, alt: "component1", src: "/images/component1.png" },
-  { id: 2, alt: "component2", src: "/images/component2.png" },
+  { id: 2, alt: "component3", src: "/images/component3.png" },
 ];
 
 export const suggestedToWatch: Movie[] = [
-  { id: 1, alt: "component3", src: "/images/component3.png" },
-  { id: 2, alt: "component4", src: "/images/component4.png" },
-  { id: 3, alt: "component5", src: "/images/component5.png" },
-  { id: 4, alt: "component6", src: "/images/component6.png" },
+  { id: 1, alt: "component4", src: "/images/component4.png" },
+  { id: 2, alt: "component5", src: "/images/component5.png" },
+  { id: 3, alt: "component6", src: "/images/component6.png" },
+  { id: 4, alt: "component7", src: "/images/component7.png" },
 ];
 
 export const previouslyWatched: Movie[] = [
@@ -22,14 +22,14 @@ export const previouslyWatched: Movie[] = [
   { id: 3, alt: "component5", src: "/images/component5.png" },
   { id: 4, alt: "component4", src: "/images/component4.png" },
   { id: 5, alt: "component3", src: "/images/component3.png" },
-  { id: 6, alt: "component2", src: "/images/component2.png" },
-  { id: 7, alt: "component1", src: "/images/component1.png" },
-  { id: 8, alt: "component7", src: "/images/component7.png" },
+  { id: 6, alt: "component1", src: "/images/component1.png" },
+  { id: 7, alt: "component7", src: "/images/component7.png" },
+  { id: 8, alt: "component4", src: "/images/component4.png" },
   { id: 9, alt: "component6", src: "/images/component6.png" },
   { id: 10, alt: "component5", src: "/images/component5.png" },
   { id: 11, alt: "component4", src: "/images/component4.png" },
   { id: 12, alt: "component3", src: "/images/component3.png" },
-  { id: 13, alt: "component2", src: "/images/component2.png" },
+  { id: 13, alt: "component7", src: "/images/component7.png" },
   { id: 14, alt: "component1", src: "/images/component1.png" },
 ];
 export const dynamicSearchData: Movie[] = [
@@ -38,13 +38,13 @@ export const dynamicSearchData: Movie[] = [
   { id: 3, alt: "component5", src: "/images/component5.png" },
   { id: 4, alt: "component4", src: "/images/component4.png" },
   { id: 5, alt: "component3", src: "/images/component3.png" },
-  { id: 6, alt: "component2", src: "/images/component2.png" },
+  { id: 6, alt: "component6", src: "/images/component6.png" },
   { id: 7, alt: "component1", src: "/images/component1.png" },
   { id: 8, alt: "component7", src: "/images/component7.png" },
   { id: 9, alt: "component6", src: "/images/component6.png" },
   { id: 10, alt: "component5", src: "/images/component5.png" },
   { id: 11, alt: "component4", src: "/images/component4.png" },
   { id: 12, alt: "component3", src: "/images/component3.png" },
-  { id: 13, alt: "component2", src: "/images/component2.png" },
+  { id: 13, alt: "component5", src: "/images/component5.png" },
   { id: 14, alt: "component1", src: "/images/component1.png" },
 ];
