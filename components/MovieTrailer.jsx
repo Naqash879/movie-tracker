2c699f73a28433a788e7066bebb17aca53ea4443
Naqash879 <naqashworkbench@gmail.com>
2025-11-28 12:03:21 +0000

    Implement Movie detail page with dynamic data that comes from API (#20)

--- components/MovieTrailer.jsx
+++ components/MovieTrailer.jsx
@@ -1,7 +1,6 @@
 function MovieTrailer({ ...props }) {
   return (
     <iframe
-      src="https://www.youtube.com/embed/BmllggGO4pM"
       title="YouTube video player"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
