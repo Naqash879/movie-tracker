3da22d3a80d93520eba447931b32595e749ca040
Naqash879 <naqashworkbench@gmail.com>
2025-12-01 14:11:01 +0000

    Add movies via API

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -3,13 +3,26 @@ import { useState } from "react";
 import { FaSearch, FaPlus } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
-import { getMovies } from "@/services/movies";
 import toast from "react-hot-toast";
+import InputField from "./InputField";
+import FormButton from "./FormButton";
+import { addMovie } from "@/services/movies";
 
 function SearchBar() {
   const [openSearchBar, setOpenSearchBar] = useState(false);
   const [value, setValue] = useState("");
+  const [movieName, setMovieName] = useState("");
+  const [movieDescription, setMovieDescription] = useState("");
+  const [moviePosterURL, setMoviePosterURL] = useState("");
+  const [movieTrailerURL, setMovieTrailerURL] = useState("");
+  const [movieRating, setMovieRating] = useState();
+  const [movieReviews, setMovieReviews] = useState();
+  const [addMovieButton, setAddMovieButton] = useState(false);
   const router = useRouter();
+  movieName.trim();
+  movieDescription.trim();
+  moviePosterURL.trim();
+  movieTrailerURL.trim();
 
   const handleSearchClick = () => {
     setOpenSearchBar((prev) => !prev);
@@ -24,8 +37,40 @@ function SearchBar() {
       router.push(`/search?searchValue=${value}`);
     }
   };
+  const handleAddMovieButton = () => {
+    setAddMovieButton((prev) => (prev = !prev));
+  };
+  const movieAdd = async (e) => {
+    e.preventDefault();
+
+    setAddMovieButton((prev) => !prev);
+
+    const res = await addMovie(
+      movieName,
+      movieDescription,
+      moviePosterURL,
+      movieTrailerURL,
+      Number(movieRating),
+      Number(movieReviews)
+    );
+
+    console.log(res);
+
+    if (res.success) {
+      toast.success("Movie Added Successfully");
+      setMovieName("");
+      setMovieDescription("");
+      setMoviePosterURL("");
+      setMovieTrailerURL("");
+      setMovieRating("");
+      setMovieReviews("");
+    } else {
+      toast.error("Movie Not Added");
+    }
+  };
+
   return (
-    <div className="w-full  px-4 mt-4 ">
+    <div className="w-full  px-4 mt-4 relative">
       <div className="flex items-center justify-between sm:hidden ">
         <h1 className="-ml-4 text-xl font-semibold shrink-0">Movie Tracker</h1>
         <div className="flex items-right gap-3">
@@ -37,7 +82,7 @@ function SearchBar() {
             <FaSearch className="text-black" />
           </button>
 
-          <button className="w-10 h-10 bg-[#D9D9D9] rounded-full flex items-center justify-center">
+          <button className="w-10 h-10 sm:flex bg-[#D9D9D9] rounded-full flex items-center justify-center">
             <FaPlus className="text-black" />
           </button>
         </div>
@@ -78,6 +123,13 @@ function SearchBar() {
               onKeyDown={handleKeyDown}
             />
           </div>
+
+          <button
+            className="w-10 h-10 ml-3 sm:flex bg-[#D9D9D9] rounded-full flex items-center justify-center"
+            onClick={handleAddMovieButton}
+          >
+            <FaPlus className="text-black" />
+          </button>
         </div>
         <div className="flex">
           <button
@@ -88,6 +140,56 @@ function SearchBar() {
           </button>
         </div>
       </div>
+      {addMovieButton && (
+        <div className=" mt-4 flex flex-col items-center shadow-3xl  sm:mt-9 absolute left-0 right-0 mx-auto w-[90%] sm:w-[70%] md:w-[50%] lg:w-[40%] xl:w-[30%] bg-white p-6 rounded-lg shadow-lg">
+          <h1 className="text-2xl">Add Movie </h1>
+          <form className="flex flex-col w-full" onSubmit={movieAdd}>
+            <InputField
+              placeholder="Movie Title"
+              type="text"
+              name={movieName}
+              value={movieName}
+              onChange={(e) => setMovieName(e.target.value)}
+            />
+            <InputField
+              placeholder="Movie Description"
+              type="text"
+              name={movieDescription}
+              value={movieDescription}
+              onChange={(e) => setMovieDescription(e.target.value)}
+            />
+            <InputField
+              placeholder="Movie PosterURL"
+              type="url"
+              name={moviePosterURL}
+              value={moviePosterURL}
+              onChange={(e) => setMoviePosterURL(e.target.value)}
+            />
+            <InputField
+              placeholder="Movie TrailerURL"
+              type="url"
+              name={movieTrailerURL}
+              value={movieTrailerURL}
+              onChange={(e) => setMovieTrailerURL(e.target.value)}
+            />
+            <InputField
+              placeholder="Movie Rating"
+              type="number"
+              name={movieRating}
+              value={movieRating}
+              onChange={(e) => setMovieRating(e.target.value)}
+            />
+            <InputField
+              placeholder="Movie Reviews"
+              type="number"
+              name={movieReviews}
+              value={movieReviews}
+              onChange={(e) => setMovieReviews(e.target.value)}
+            />
+            <FormButton>Add Movie</FormButton>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
